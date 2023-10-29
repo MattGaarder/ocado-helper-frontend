0d0667f483e5a0966deb46ec58f880cf214043f9
Matt Gaarder <matteus.gaarder@gmail.com>
2023-10-29 11:54:30 +0000

    fixes path backend from null

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  * @param {PDFDocument} PDFDocumentInstance The PDF document obtained 
 **/
 
-PDF_URL = `https://sleepy-hamlet-41974-03ab43335a23.herokuapp.com/${response.data.filePath}`;
+PDF_URL = null;
 
 
 // https://sleepy-hamlet-41974-03ab43335a23.herokuapp.com
@@ -19,7 +19,7 @@ document.getElementById("pdfForm").addEventListener("submit", (event) => {
         }
     }).then(response => {
         console.log('File uploaded successfully');
-        PDF_URL = `/${response.data.filePath}`;
+        PDF_URL = `https://sleepy-hamlet-41974-03ab43335a23.herokuapp.com${response.data.filePath}`;
         fetch('./openfoodfacts.json')
     .then(response => response.json())
     .then(data => {
